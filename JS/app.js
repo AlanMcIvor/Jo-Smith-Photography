c0418c680abc79aa6d29e8f31b58465b4edb4046
Alan McIvor <alan.mcivor27@gmail.com>
2023-10-04 15:41:02 +0100

    altered some javascript

--- JS/app.js
+++ JS/app.js
@@ -2,9 +2,11 @@
 //   variables
   const menuButton = document.querySelector('.menu_btn');
   const navList = document.querySelector('.nav_list');
+  const body = document.querySelector('body');
 
 //  event listeners
   menuButton.addEventListener("click", () => {
     // when hamburger clicked add or remove the active class
     navList.classList.toggle("active");
+    body.classList.toggle("noScroll");
   } )
