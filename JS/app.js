a104683f2d5b332d0645aa310fdc3886cabc2f92
Alan McIvor <alan.mcivor27@gmail.com>
2023-10-15 21:59:25 +0100

    added some jquery

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,15 @@
-const navbar = document.querySelector(".navbar");
-const burger = document.querySelector(".burger");
 
+// variables
+const sidebar = document.querySelector(".sidebar");
+const burger = document.querySelector(".bx");
+
+// event listners
+
+// when burger clicked show/hide nav
 burger.addEventListener("click", function(){
-  navbar.classList.toggle("moveIn");
+ sidebar.classList.toggle("move_in");
 })
+
+
+// jquery to show images full screen upon click
+$(document).ready(function(){$("img").click(function(){this.requestFullscreen()})});
