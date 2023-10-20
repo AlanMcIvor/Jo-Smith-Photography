9c7bee1f27b8d9d629b53905a42c01d6d1f62974
Alan McIvor <alan.mcivor27@gmail.com>
2023-10-20 17:38:35 +0100

    lazy loading

--- JS/app.js
+++ JS/app.js
@@ -3,13 +3,13 @@
 const sidebar = document.querySelector(".sidebar");
 const burger = document.querySelector(".burger");
 
-// event listners
+// event listneres
 
-// when burger clicked show/hide nav
 burger.addEventListener("click", function(){
+    // when burger clicked show/hide nav
  sidebar.classList.toggle("move_in");
 })
 
 
-// jquery to show images full screen upon click
+// jQuery to show images full screen upon click
 $(document).ready(function(){$("img").click(function(){this.requestFullscreen()})});
