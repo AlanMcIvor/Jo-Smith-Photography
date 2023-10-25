247434096be9dacc9f4db5d8d8a57d611ca4cc7e
alan mcivor <alan.mcivor27@gmail.com>
2023-10-25 11:58:49 +0100

    created a few sections

--- JS/app.js
+++ JS/app.js
@@ -2,9 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navbar.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
 
 // jQuery to show images full screen upon click
-$(document).ready(function(){$("img").click(function(){this.requestFullscreen()})});
+$(document).ready(function () {
+  $("img").click(function () {
+    this.requestFullscreen();
+  });
+});
