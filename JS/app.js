ec1a36d02396c0365151bc94a99833d145cc8550
Alan McIvor <alan.mcivor27@gmail.com>
2023-10-28 16:53:06 +0100

    created js filter

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,34 @@
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
+const wedding_btn = document.querySelector("#wedding_btn");
+const landscape_btn = document.querySelector("#landscape_btn");
+const wedding_img = document.querySelectorAll(".wedding");
+const landscape_img = document.querySelectorAll(".landscape");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navbar.classList.toggle("active");
 });
+
+// when wedding button clicked hide all landscape images
+
+wedding_btn.addEventListener("click", (e) => {
+  e.preventDefault()
+    landscape_img.forEach(img => {
+      img.classList.toggle("img_toggle")
+    })
+
+})
+
+
+// when landscape button clicked hide all landscape images
+landscape_btn.addEventListener("click", (e) => {
+  e.preventDefault()
+  wedding_img.forEach(img => {
+    img.classList.toggle("img_toggle")
+  })
+
+})
 
 // jQuery to show images full screen upon click
 $(document).ready(function () {
